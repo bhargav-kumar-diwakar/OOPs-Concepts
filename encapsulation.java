3e8fca8a5961bb08d44b0255f9ceccebc1f10b09
bhargav-kumar-diwakar <bhargavkumardiwakar@gmail.com>
2026-02-08 10:35:31 +0530

    Add vechile class demonstrating encapsulation

---
+++ encapsulation.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+class Vechile{
+    private String brand;
+    private int speed;
+
+    //setter for brand
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+
+    //setter for speed
+    public void setSpeed(int speed){
+        if(speed<0){
+            System.out.println("Speed cannot be negative!");
+            this.speed = 0;
+        }else{
+            this.speed = speed;
+        }
+    }
+
+    //getter for brand
+    public String getBrand(){
+        return brand;
+    }
+
+    //getter for speed
+    public int getSpeed(){
+        return speed;
+    }
+}
