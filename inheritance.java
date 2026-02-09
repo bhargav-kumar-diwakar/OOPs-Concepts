0ba22f0cc3955e93ec744d25433941d032162b57
bhargav-kumar-diwakar <bhargavkumardiwakar@gmail.com>
2026-02-09 23:05:43 +0530

    Added vechile class demonstrating inheritance

---
+++ inheritance.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+class Vechile{
+    void fuelType(){
+        System.out.println("Vechile uses petrol as fuel.");
+    }
+}
+
+class car extends Vechile{
+    void seatingCapacity(){
+        System.out.println("Car has a seating capacity of 5 people.")
+    }
+}
+
